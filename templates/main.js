fd11e860a03ee5fffcdd51a570bd48a10c4896ef
Maltonn <ymatsubayashi09@outlook.jp>
2023-10-27 17:03:02 +0900

    udpate

--- templates/main.js
+++ templates/main.js
@@ -1,92 +1,141 @@
+DIRECTION_MODE="total"
 
-
-async function Refresh() {
-    return fetch('log/').then((response)=> {
-        return response.json();
-    }
-    ).then((data)=> {
-        return data;
-    }
-    );
+async function GetLogData() {
+  return fetch('log/').then((response) => {
+    return response.json();
+  }
+  ).then((data) => {
+    return data;
+  }
+  );
 }
 
-setInterval(async () => {
-    const table = {};
-    const data = await Refresh();
-    data.forEach(({ timestamp, direction }) => {
-        const date = new Date(timestamp * 1000);
-        const day = date.toLocaleDateString();
-        const hour = date.getHours();
+function zeros(a, b) {
+  let result = new Array(a);
 
-        if (!table[direction]) {
-            table[direction] = {};
-        }
+  for (let i = 0; i < a; i++) {
+    result[i] = new Array(b).fill(0);
+  }
 
-        if (!table[direction][day]) {
-            table[direction][day] = {};
-        }
+  return result;
+}
 
-        if (!table[direction][day][hour]) {
-            table[direction][day][hour] = 0;
-        }
 
-        table[direction][day][hour]++;
-    });
+const Refresh = async () => {
+  const data = await GetLogData();
+  const days = []
+  const sections = []
 
+  data.forEach(({ timestamp, direction }) => {
+    const date = new Date(timestamp * 1000);
+    const day = date.toLocaleDateString();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const section=hour+(minute<30?":00":":30")
+    const dayIndex = days.indexOf(day);
+    const sectionIndex = sections.indexOf(hour);
     
-    console.table(table);
-    CreateTable(table)
-}, 10000);
+    if (!days.includes(day)) {
+      days.push(day);
+    }
+    if (!sections.includes(section)) {
+      sections.push(section);
+    }
+  });
 
-function CreateTable(table) {
-    document.getElementById('main_table').outerHTML = '';
+  days.sort((a, b) => new Date(a) - new Date(b));
+  sections.sort((a, b) => a.localeCompare(b, undefined, {numeric: true}));
 
-    const tableElement = document.createElement('table');
-    tableElement.id='main_table'
-    const theadElement = document.createElement('thead');
-    const tbodyElement = document.createElement('tbody');
+  tablesDict = {
+    "to_right": zeros(days.length, sections.length),
+    "to_left": zeros(days.length, sections.length),
+    "unknown": zeros(days.length, sections.length),
+    "total": zeros(days.length, sections.length),
+  }
 
-    // Create table header
-    const headerRow = document.createElement('tr');
-    const headerCell1 = document.createElement('th');
-    headerCell1.textContent = 'Direction';
-    const headerCell2 = document.createElement('th');
-    headerCell2.textContent = 'Day';
-    const headerCell3 = document.createElement('th');
-    headerCell3.textContent = 'Hour';
-    const headerCell4 = document.createElement('th');
-    headerCell4.textContent = 'Count';
+  data.forEach(({ timestamp, direction }) => {
+    const date = new Date(timestamp * 1000);
+    const day = date.toLocaleDateString();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const section=hour+(minute<30?":00":":30")
+    const dayIndex = days.indexOf(day);
+    const sectionIndex = sections.indexOf(section);
 
-    headerRow.appendChild(headerCell1);
-    headerRow.appendChild(headerCell2);
-    headerRow.appendChild(headerCell3);
-    headerRow.appendChild(headerCell4);
-    theadElement.appendChild(headerRow);
 
-    // Create table body
-    for (const direction in table) {
-        for (const day in table[direction]) {
-            for (const hour in table[direction][day]) {
-                const row = document.createElement('tr');
-                const cell1 = document.createElement('td');
-                cell1.textContent = direction;
-                const cell2 = document.createElement('td');
-                cell2.textContent = day;
-                const cell3 = document.createElement('td');
-                cell3.textContent = hour;
-                const cell4 = document.createElement('td');
-                cell4.textContent = table[direction][day][hour];
-
-                row.appendChild(cell1);
-                row.appendChild(cell2);
-                row.appendChild(cell3);
-                row.appendChild(cell4);
-                tbodyElement.appendChild(row);
-            }
-        }
+    dic = {
+      ">": "to_right",
+      "<": "to_left",
+      "?": "unknown",
     }
 
-    tableElement.appendChild(theadElement);
-    tableElement.appendChild(tbodyElement);
-    document.body.appendChild(tableElement);
+    tablesDict[dic[direction]][dayIndex][sectionIndex]++;
+    tablesDict["total"][dayIndex][sectionIndex]++;
+  });
+
+  CreateTable(days, sections, tablesDict, DIRECTION_MODE)
+  console.log("table created")
 }
+
+
+function CreateTable(days, sections, tablesDict, mode) {
+  document.getElementById('table')?.remove();
+  const table = tablesDict[mode];
+  const tableElement = document.createElement('table');
+  tableElement.id = 'table';
+  const theadElement = document.createElement('thead');
+  const tbodyElement = document.createElement('tbody');
+
+  // Create table header row
+  const headerRow = document.createElement('tr');
+  headerRow.appendChild(document.createElement('th'));
+  for (let i = 0; i < days.length; i++) { // Inverse days and sections
+    const day = days[i];
+    const dayCell = document.createElement('th');
+    dayCell.textContent = day;
+    headerRow.appendChild(dayCell);
+  }
+  theadElement.appendChild(headerRow);
+
+  // Create table body rows
+  for (let i = 0; i < sections.length; i++) { // Inverse days and sections
+    const hour = sections[i];
+    const hourRow = document.createElement('tr');
+    const hourCell = document.createElement('th');
+    hourCell.textContent = hour;
+    hourRow.appendChild(hourCell);
+    for (let j = 0; j < days.length; j++) { // Inverse days and sections
+      const day = days[j];
+      const count = table[j][i]; // Inverse days and sections
+      const countCell = document.createElement('td');
+      countCell.textContent = count;
+      hourRow.appendChild(countCell);
+    }
+    tbodyElement.appendChild(hourRow);
+  }
+
+  tableElement.appendChild(theadElement);
+  tableElement.appendChild(tbodyElement);
+  document.body.appendChild(tableElement);
+}
+
+
+const radioButtons = document.querySelectorAll('input[name="direction_option"]');
+radioButtons.forEach(radioButton => {
+  radioButton.addEventListener('change', ()=>{
+    const selectedFruitElement = document.querySelector('input[name="direction_option"]:checked');
+    if (selectedFruitElement) {
+      DIRECTION_MODE=selectedFruitElement.value
+      Refresh()
+    }
+  });
+});
+
+
+
+
+window.onload =()=>{
+  Refresh()
+}
+
+setInterval(Refresh, 1000 * 10)
